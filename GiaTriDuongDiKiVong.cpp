e7995e1396b145c901ffb965d80ce285c43e1665
Nguyễn Thanh Châu <69909023+thanhchauns2@users.noreply.github.com>
2022-01-23 22:55:35 +0700

    Create GiaTriDuongDiKiVong.cpp

---
+++ GiaTriDuongDiKiVong.cpp
@@ -0,0 +1,405 @@
+// Designed by Nguyen Thanh Chau
+// a.k.a Paperback Collector
+// En Taro Adun! - TemplarAssasin a.k.a Zeratul
+
+//cout<<"                  :/+ossyyhhhhhhhhhhhhhhhhysooooooo+//:..`                                   "<<endl;
+//cout<<"        `dNNy+/`--:/+oooyMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNho+/:.`           ...        "<<endl;      
+//cout<<"       :hhoNN+`//:..```.MMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMd``..:/ooooo++/ oyyNNN+            "<<endl; 
+//cout<<"        :hhoNN/``/+ooo+o+++/::...``````````.-://-+so/://+ohsyhdmmsooo+/:-.````` hMsod/m`            "<<endl;
+//cout<<"        `oNMMMNh:-.``             `.-.-/+++/-.:ymNMNNmy/..`  ```./o+/..-/+ooo+/ hM/sm:s`            "<<endl;
+//cout<<"          :mMMMMNo`            -+hms/:-.`   `:+oMMhssyhs`          .:::o+//:-`` `/y:MMM/            "<<endl;
+//cout<<"           -dMMMMMd:        ./ohNh-           :mMs`                 :dd-```.-/+:` d`NMMd           "<<endl; 
+//cout<<"            `yMMMMMNd.   `/+//dMy`           :NMo`                `oNm.       `-++o-yMMM:           "<<endl;
+//cout<<"             `oNMMMMs: .+o-`/NMs`   `.-::///smMh::-.`            `yMN-           `oNmMMmh           "<<endl;
+//cout<<"               /NMMMy/+o-  +NMs`-/shmmmmddddhddddmmmmho/-`      `hMN/             /hoMd.N.          "<<endl;
+//cout<<"                +NMMNs-   /NMN/dNmds+:-.```` .y``.-:+sdmNds:.   yMMy              .--Ns sy.         "<<endl;
+//cout<<"                 +Md:    .NMMm/s:.`          y:       `.:sdNms-/MMN-              -dNMs  /h/        "<<endl;
+//cout<<"               .oms`   `//MNy:`             /s            `:smhyMMh                .ymh`  .ho       "<<endl;
+//cout<<"              .dm:`   :dN/o.               .h`               .:sMMs                  .`    `ho`     "<<endl;
+//cout<<"              sm-   `yNNs`          ``.... o/..`               .sN+y.                       sMy.    "<<endl;
+//cout<<"             +m-   -dMd-        `-/o+/:---`.`-://:.`             -:Mm:                     :MMMm-   "<<endl;
+//cout<<"            /N/   :mMh`       -+oyo/::- `..````  `://.            `yMN:                    mMMMMd   "<<endl;
+//cout<<"           -Ny   :NMy`      -yNMMMMMNdsymNmmdhhyoo/:/+-`           `sMN/                  :MMMMMN`  "<<endl;
+//cout<<"           hN.  .mMy      `sNMMMMMNdydMMMMsMmoMyhMhNMNmm+            yMN.                 dMMMMMM`  "<<endl;
+//cout<<"          :Ms   yMd`     .dMMMMMMmymMMMmyMshm/N:NshNhhMMMs`          `mMd                .MMMMMMM`  "<<endl;
+//cout<<"          hM.  -MM/     `dMMMMMMmyMMMmsNh+sshhhhy+yodmhhMMy           /MM/               /MMMMMMM`  "<<endl;
+//cout<<"         `Nd   yMd      oMMMMMMNsMMMhmmoohMMMMMMMMms+ymNdmM+           dMd               yMMMMMMM`  "<<endl;
+//cout<<"         -Ms  `NMs      mMMMMMMhmMMMNhs/NMMMMMMMMMMMosyhmMMN`          oMM`             .NMMMMMMM`  "<<endl;
+//cout<<"         -M+  -MM/     .MMMMMMM+MMMdhh+hMMMMMMMMMMMMm+yyhhMM/          :MM:             oMMMMMMMm   "<<endl;
+//cout<<"         /M:  -MM:     :MMMMMMMsMMMhhy+dMMMMMMMMMMMMm/yyhhMM+          -MM/             yMMMMMMMm   "<<endl;
+//cout<<"         +M+  -MM/     /MMMMMMMymMMMNysoMMMMMMMMMMMM+ohNMNMM+          :MM:             mMMMMMMMy   "<<endl;
+//cout<<"         .My  .MM+     -MMMMMMMNyMMNdNd+omMMMMMMMMmo+mmyyNMM:          /MM-            `NMMMMMMMo   "<<endl;
+//cout<<"          my   mMh      mMMMMMMMdyMMMhhNoo+yyhhhssosNyymMhNm           yMN`            .MMMMMMMM:   "<<endl;
+//cout<<"          om   oMN.     omMMMMMMMdhNMMMymN/N+dyym+NdsmMNyhM:          `NMs             /MMMMMMMM.   "<<endl;
+//cout<<"          `m:  `NMy      /y:/oydmNNydNMMMdmMoMN+MmhMNNhymM+           sMN.             /MMMMMMMm    "<<endl;
+//cout<<"           :d`  +MM+     `sd:  ```-:..+hdmNNNMMmMMdyyymMN/           /MMo              oMMMMMMMs    "<<endl;
+//cout<<"            yo   yMN:      /hh-         ```..-::/.`:osys-           -NMh`              oMMMMMMM/    "<<endl;
+//cout<<"            .m-  `hMN:      `+hs:`                 `-+.            :mMh`               oMMMMMMN`    "<<endl;
+//cout<<"             /d`  `yMN-       `-sys/-           .-:/.`            /NMy`                oMMMMMMy     "<<endl;
+//cout<<"             `hh`  `+m:h-        `./+`h--++/://:-.`             .yMNo`                 oMMMMMM:     "<<endl;
+//cout<<"            -dMMh`   .sMN:`           N-``.```                 +mMd:                   /MMMMMd`     "<<endl;
+//cout<<"          -sNMMMds-   oMM/ys-         d:                    -o+dd+`                    -MMMMN:      "<<endl;
+//cout<<"        .sNMMMMN/.:+` `mM/hNNh+.`     y/                 .//NMs-`                      `MMNy-       "<<endl;
+//cout<<"      .sNMMMMMNms  ./-`:Nd.-odNNds/.` o+            .-:ohNNoMMs                        .o/-         "<<endl;
+//cout<<"    `omMMMMMNy:.     -/:+Nh` `-+hmNNmhdd+/::::://oyhmNNmdo:/MMy               `.+o-  `+y-           "<<endl;
+//cout<<"  `/dMMMMNdo-          ./hMy.    `-:+shdNMNmmmmmmdhyo:-`   /MMh               +mMd../y/`            "<<endl;
+//cout<<"  /MMohmds:o/:.`         `omm/.`        sMm-....`          `NMN.           ...-+M+`d+`              "<<endl;
+//cout<<"  +MMsys-``.-/+os+/:-.``   .//://.`     `dMy                :NMs        `./yydyhM-+y                "<<endl;
+//cout<<"  :ydh+++-:os+-..-dMMNdhs+/-.```.://.`  /sMMs---.            -dN-   `.:+/:. -ymMMsm:                "<<endl;
+//cout<<"            `-/ossmMMMMMMMMMNmmhyso+ooo/-:osyyo/.           ``.oh:/+/:.    .s.NMMN-                 "<<endl;
+//cout<<"                 `/shmNNMMMMMMMMMMMMMMMMMmddhyyss++/////osoooooo+/:--.``  `h-hMMN/                  "<<endl;
+//cout<<"                      .-/+shdmNNNMMMMMMMMMMMMMMMMMMMMMMMMh:::::::::::++o:yh+oNNMo                   "<<endl;
+//cout<<"                              `.-:/osyhddNNNNMMMMMMMMMMMMo-----------.``-Md s/:o`                   "<<endl;
+//cout<<"                                          ``.:://++oooyyo+::-------:/+o+:Nmomsd/                    "<<endl;
+//cout<<"                                                                         `.ydh.                     "<<endl;
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                    //
+//                                Libraries and stuffs                                //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Libraries
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+
+// Defines
+
+	using ll = long long;
+	using ld = long double;
+	using ull = unsigned long long;
+	#define pb push_back
+	#define eb emplace_back
+	#define f first
+	#define s second
+	#define p pair
+	#define all(C) C.begin(), C.end()
+	#define Toggle() int N; cin>>N; cin.ignore(); while(N--)
+	#define SZ(a) (ll)a.size()
+	#define FOR(i,a,b) for (ll i=(ll)a; i<=(ll)b; i++)
+	#define FOR2(i,a,b) for (ll i=(ll)a; i<=(ll)b; ++i)
+	#define FORD(i,a,b) for (ll i=(ll)a; i>=(ll)b; i--)
+	#define reset(C) memset(C,0,sizeof(C))
+	#define vec vector
+	#define Rubik(x) Matrix<vector<x> >
+	#define faster() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+// Namespaces
+
+using namespace std;
+using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                    //
+//                               A few extra functions                                //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
+
+multiset<ll> LIS(vector<ll> A)
+{
+	ll a = A.size();
+	multiset<ll> S;
+	multiset<ll>::iterator it;
+	FOR(i, 0, a - 1)
+	{
+		S.insert(A[i]);
+		it = S.upper_bound(A[i]);
+		if (it != S.end()) S.erase(it);
+	}
+	return S;
+}
+
+multiset<ll> LSIS(vector<ll> A) // a.k.a Longest Strictly Increasing Sequence
+{
+	ll a = A.size();
+	multiset<ll> S;
+	multiset<ll>::iterator it;
+	FOR(i, 0, a - 1)
+	{
+		S.insert(A[i]);
+		it = S.lower_bound(A[i]);
+		it++;
+		if (it != S.end()) S.erase(it);
+	}
+	return S;
+}
+
+ll gcd(ll a, ll b) {if (a == 0) return(b); return(gcd(b % a, a));}
+
+ll extGcd(ll a, ll b, ll& x, ll& y) // returns d=gcd(a,b), and give one pair x,y such that ax+by=d
+{
+	if(a==0)
+	{
+		x=0;y=1;
+		return(b);
+	}
+	ll a1, b1, c, x1, y1, rst;
+	a1 = b % a; b1 = a; c = b / a;
+	rst = extGcd(a1, b1, x1, y1);
+	x = y1 - c * x1; y = x1;
+	return(rst);
+}
+
+ll powermod(ll a, ll b, ll m)
+{
+	if (b == 0) return 1;
+	ull k = powermod(a, b / 2, m);
+	k = k * k;
+	k %= m;
+	if (b & 1) k = (k * a) % m;
+	return k;
+}
+
+// Prime test for large numbers
+
+bool isPrime(ull n, int iter = 5) 
+{
+    if (n < 4) return n == 2 || n == 3;
+	if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 0; i < iter; i++) 
+	{
+        ull a = 2 + rand() % (n - 3);
+        if (powermod(a, n - 1, n) != 1) return false;
+    }
+    return true;
+}
+
+// Mod int
+
+struct Mint
+{
+	ll MOD = 1e9 + 7;
+	ll val;
+	bool operator==(const Mint& other) {return val == other.val;}
+	Mint operator+(const Mint& other) {ll res = val + other.val; while(res >= MOD) res -= MOD; while(res < 0) res += MOD; return Mint(res);}
+	Mint operator-(const Mint& other) {ll res = val - other.val; while(res >= MOD) res -= MOD; while(res < 0) res += MOD; return Mint(res);}
+	Mint operator*(const Mint& other) {return Mint((val * 1ll * other.val) % MOD);}
+	Mint operator+=(const Mint& other) {val += other.val; while(val >= MOD) val -= MOD; while(val < 0) val += MOD; return val;}
+	Mint operator-=(const Mint& other) {val -= other.val; while(val >= MOD) val -= MOD; while(val < 0) val += MOD; return val;}
+	Mint operator*=(const Mint& other) {return val = (val * 1ll * other.val) % MOD;}
+	Mint pow(ll y) {Mint z(1); Mint x(val); while(y > 0) {if(y % 2 == 1) z = z * x; x = x * x; y /= 2;} return z;}
+	Mint operator/(const Mint& other) {return Mint(val) * Mint(other.val).pow(MOD - 2);}
+	Mint() {val = 0;};
+	Mint(ll x) {x %= MOD; while(x < 0) x += MOD; val = x;};
+};
+
+ostream& operator<<(ostream& out, const Mint& x) {return out << x.val;}
+
+// End of function
+
+
+// Improve some STD functions
+
+template<typename T> void getVector(T &a) {for (auto &x : a) cin >> x;}
+template<typename T> void printVector(T &a) {for (auto &x: a) cout << x << ' ';}
+template<typename T> void revrs(T &a) {reverse(a.begin(), a.end());}
+template<typename T> void SORT(T &a) {sort(a.begin(), a.end());}
+template<typename T> void SORTD(T &a) {sort(a.rbegin(), a.rend());}
+#define SORTC(a, cmp) sort(a.begin(), a.end(), cmp)
+template<typename T> T maxOf(T &a) {return max_element(a.begin(), a.end()) - a.begin();}
+template<typename T> T minOf(T &a) {return min_element(a.begin(), a.end()) - a.begin();}
+template<typename T> T Merge(T &a, T &b) {T c; merge(all(a), all(b), c.begin()); return c;}
+template<typename T> void Fill(vector<T> &a, T k) {fill(a.begin(), a.end(), k);}
+template<typename T> void Fill(deque<T> &a, T k) {fill(a.begin(), a.end(), k);}
+template<typename T> void tlw(T &a) {for(int i=0; i<(sizeof(a)/sizeof(a[0])); i++) a[i] = tolower(a[i]);}
+template<typename T> void tup(T &a) {for(int i=0; i<(sizeof(a)/sizeof(a[0])); i++) a[i] = toupper(a[i]);}
+template<typename T> void Unique(T &a) {a.erase(unique(a.begin(), a.end()), a.end());}
+template<typename T1, typename T2> ostream& operator<<(ostream& out, const p<T1, T2>& x) {return out << x.f << ' ' << x.s;}
+template<typename T1, typename T2> istream& operator>>(istream& in, p<T1, T2>& x) {return in >> x.f >> x.s;}
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, vector<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+template<typename T> istream& operator>>(istream& in, deque<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, deque<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+template<typename T> using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T> using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T> using pq = priority_queue<T>;
+template<typename T> using reverse_pq = priority_queue<T, vector<T>, greater<T> >;
+template<typename T> using matrix = vector<vector<T> >;
+template<typename T> using rubik = vector<vector<vector<T> > >;
+
+// Matrix
+
+
+
+template<typename T> matrix<T> operator * (const matrix<T> &a, const matrix<T> &b) {
+    if (SZ(a.x) == 0 || SZ(b.x) == 0) return matrix<T>(); // not sure if make sense?
+
+    matrix<T> c(SZ(a.x), SZ(b[0]));
+    FOR(i, 0, SZ(c.x) - 1) FOR(j, 0, SZ(c[0]) - 1)
+    {
+        FOR(k, 0, SZ(a[0]) - 1)
+            c[i][j] += a[i][k] * b[k][j];
+    }
+    return c;
+}
+
+template<typename T> matrix<T> inverse(matrix<T> a) 
+{
+    assert(SZ(a.x) == SZ(a[0]));
+    int n = SZ(a.x);
+
+    FOR(i, 0, n - 1)
+	{
+        vector<T> tmp(n);
+        tmp[i] = 1;
+        a[i].insert(a[i].end(), tmp.begin(), tmp.end());
+    }
+
+    a.gauss();
+
+    matrix<T> inv(n, n);
+    FOR(i, 0, n - 1) FOR(j, 0, n - 1)
+	{
+        inv[i][j] = a[i][j+n];
+    }
+    return inv;
+}
+
+// End of function
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                    //
+//                                     Constants                                      //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
+
+ld PI = 3.14159265358979323846;
+
+ld eps = 1e-6;
+
+//ll M = 1e9 + 7;
+
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                    //
+//                     End of preparation, your code starts HERE                      //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
+
+bool __isprime(ll x)
+{
+	if (x < 2) return false;
+	if (x == 2) return true;
+	if (x % 2 == 0) return false;
+	if (x == 3) return true;
+	if (x % 3 == 0) return false;
+	ll t = 1;
+	for (ll i = 5; i * i <= x; i += 2 * (1 + t))
+	{
+		if (x % i == 0) return false;
+		t = 1 - t;
+	}
+	return true;
+}
+
+int Gauss(matrix<ld> a, vector<ld> &ans) {
+  int n = (int)a.size(), m = (int)a[0].size() - 1;
+  vector<int> pos(m, -1);
+  double det = 1; int rank = 0;
+  for(int col = 0, row = 0; col < m && row < n; ++col) {
+    int mx = row;
+    for(int i = row; i < n; i++) if(fabs(a[i][col]) > fabs(a[mx][col])) mx = i;
+    if(fabs(a[mx][col]) < eps) {det = 0; continue;}
+    for(int i = col; i <= m; i++) swap(a[row][i], a[mx][i]);
+    if (row != mx) det = -det;
+    det *= a[row][col];
+    pos[col] = row;
+    for(int i = 0; i < n; i++) {
+      if(i != row && fabs(a[i][col]) > eps) {
+        double c = a[i][col] / a[row][col];
+        for(int j = col; j <= m; j++) a[i][j] -= a[row][j] * c;
+      }
+    }
+    ++row; ++rank;
+  }
+  ans.assign(m, 0);
+  for(int i = 0; i < m; i++) {
+    if(pos[i] != -1) ans[i] = a[pos[i]][m] / a[pos[i]][i];
+  }
+  for(int i = 0; i < n; i++) {
+    double sum = 0;
+    for(int j = 0; j < m; j++) sum += ans[j] * a[i][j];
+    if(fabs(sum - a[i][m]) > eps) return -1; //no solution
+  }
+  for(int i = 0; i < m; i++) if(pos[i] == -1) return 2; //infinte solutions
+  return 1; //unique solution
+}
+
+void Process() 
+{
+	ll N, st, en;
+	cin >> N >> st >> en;
+	matrix<ld> C(N, vec<ld> (N + 1));
+	FOR(i, 0, N - 1)
+	{
+		C[i][i] = 1;
+		C[i][N] = 1;
+	}
+	C[en - 1][N] = 0;
+	ld save[N + 5];
+	reset(save);
+	FOR(i, 0, N - 2)
+	{
+		ll x, y; ld z;
+		cin >> x >> y >> z;
+		x--;
+		y--;
+		save[x] += z;
+		save[y] += z;
+		C[x][y] = -z;
+		C[y][x] = -z;
+	}
+	FOR(i, 0, N - 1)
+	{
+		FOR(j, 0, N - 1)
+		{
+			if (i == j) continue;
+			C[i][j] /= save[i];
+		}
+	}
+	FOR(i, 0, N) C[en - 1][i] = 0;
+	C[en - 1][en - 1] = -1;
+	vec<ld> ans(N);
+	int k = Gauss(C, ans);
+	cout << fixed << setprecision(5) << ans[st - 1] << '\n';
+}
+
+signed main() 
+{
+	faster();
+	Toggle()
+		Process();
+}
+
+//  Geometric art by Daniel yuan
+//  Redraw by Nguyen Thanh Chau
+/*   
+                ________
+               /        \
+              / /      \ \
+     ________/ /        \ \________
+    /        \            /        \ 
+   / /      \ \  ______  / /      \ \
+  / /        \ \________/ /        \ \ 
+  \            /        \            /
+   \  ______  / /      \ \  ______  /	
+    \________/ /        \ \________/
+    /        \            /        \
+   / /      \ \  ______  / /      \ \   
+  / /        \ \________/ /        \ \
+  \            /        \            /    
+   \  ______  / /      \ \  ______  /    
+    \________/ /        \ \________/  
+             \            /         
+              \  ______  / 
+               \________/   	
+*/
+////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                    //
+//                               Coded by thanhchauns2                                //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
